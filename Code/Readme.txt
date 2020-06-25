e2209f02712c874a70e865eed759c9c58c775cfc
tusharahuja0798 <52648563+tusharahuja0798@users.noreply.github.com>
2020-06-25 13:47:32 +0530

    Add files via upload

---
+++ Code/Readme.txt
@@ -0,0 +1,17 @@
+Ancient Text Translator
+
+Install all the dependencies, download the code file, the dataset, the encoder decoder models and then open cmd
+To install dependencies-
+!pip install python
+!pip install flask
+!pip install pytorch
+!pip install request
+!pip install pandas
+!pip install numpy
+!pip install nltk
+!pip install matplotlib
+!pip install sklearn
+
+execute the command- python api.py
+then go to google chrome and open the login page.
+Type in your 'Sundanese Text' and the translator will translate it for you.
---
+++ Code/api.py
@@ -0,0 +1,287 @@
+from __future__ import unicode_literals, print_function, division
+
+import numpy as np 
+import pandas as pd
+
+from flask import Flask, redirect, url_for, request,render_template
+from io import open
+import unicodedata
+import string
+import re
+import random
+import matplotlib
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from sklearn.feature_extraction.text import CountVectorizer
+device='cpu'
+
+SOS_token = 0
+EOS_token = 1
+
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  # Counting start os sentence and end 
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s) #normal form composed
+        if unicodedata.category(c) != 'Mn')
+
+
+def normalizeString(s):
+    s = s.lower().strip()
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[.!?]+", r" ", s)
+    return s
+
+
+def readLangs(lang1, lang2, reverse=True):
+    lines = open('%s-%s.txt' % (lang1, lang2),encoding='utf-8').read().strip().split('\n')  
+
+    pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+
+    return input_lang, output_lang, pairs
+
+
+MAX_LENGTH = 22
+
+def filterPair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and         len(p[1].split(' ')) < MAX_LENGTH 
+
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+
+def prepareData(lang1, lang2, reverse=False):
+    input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
+    pairs = filterPairs(pairs)
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    return input_lang, output_lang, pairs
+
+
+input_lang, output_lang, pairs = prepareData('sun', 'eng', False) 
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+
+def tensorsFromPair(pair):
+    input_tensor = tensorFromSentence(input_lang, pair[0])
+    target_tensor = tensorFromSentence(output_lang, pair[1])
+    return (input_tensor, target_tensor)
+
+
+def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
+    with torch.no_grad():
+        input_tensor = tensorFromSentence(input_lang, sentence)
+        input_length = input_tensor.size()[0]
+        encoder_hidden = encoder.initHidden()
+
+        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[ei],
+                                                     encoder_hidden)
+            encoder_outputs[ei] += encoder_output[0, 0]
+
+        decoder_input = torch.tensor([[SOS_token]], device=device) 
+
+        decoder_hidden = encoder_hidden
+
+        decoded_words = []
+        decoder_attentions = torch.zeros(max_length, max_length)
+
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
+            decoder_attentions[di] = decoder_attention.data
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_token:
+                decoded_words.append('')
+                break
+            else:
+                decoded_words.append(output_lang.index2word[topi.item()])
+
+            decoder_input = topi.squeeze().detach()
+
+        return decoded_words, decoder_attentions[:di + 1]
+
+
+encoder=torch.load('encoder.pt')
+decoder=torch.load('decoder.pt')
+
+g=open('s.txt',encoding="utf8")
+s_text=g.read()
+s_text=[s_text]
+vectorizer = CountVectorizer()
+s_vec = vectorizer.fit_transform(s_text)
+s_sep=vectorizer.get_feature_names()
+
+
+def evaluateText(encoder, decoder, string):
+	output_words, attentions = evaluate(encoder, decoder, string)
+	output_sentence = ' '.join(output_words)
+	return output_sentence
+	
+def predict(s):
+	s=s.split(' ')
+	b=[]
+	c=[]
+	for i in s:
+		if i in s_sep:
+			b.append(i)
+		else:
+			c.append(i)
+	b=' '.join(b)
+	c=' '.join(c)
+	result=evaluateText(encoder, decoder,b)
+	result=c+' '+result
+	return result
+	
+def spliter(s):
+    s=s.split(' ')
+    i=0
+    if(i==0):
+        res=''
+    c=[]
+    for j in s:
+        i=i+1
+        if(i%5==0):
+            s=' '.join(c)
+            s=predict(s)
+            res=res+s+' '
+            c=[]
+        c.append(j)
+    return res
+	
+def gang(string):
+	return string+string+string
+
+app = Flask(__name__)
+
+@app.route('/success/<n>')
+def success(n):
+	final_text=predict(n)
+	return result(final_text)
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+	sentence = request.form['nm']
+	return redirect(url_for('success',n = sentence))
+
+def result(k):
+   return render_template('output.html', result = str(k))
+
+if __name__ == '__main__':
+   app.run(debug = True)
